eced89a6cbfa2d4a0c8a4d5fe3a4d91ca88944ab
Afrizzal P. Pratama <apratama8@gmail.com>
2021-07-08 19:59:16 +0700

    Fix Operator Concatenation

--- StringChar/main.swift
+++ StringChar/main.swift
@@ -10,11 +10,11 @@ print("Simply cli app to identify string and character")
 print("Let's try!")
 print("----------------------------------")
 
-// Digunakan untuk mengambil data inputan.
+// Form Input.
 print("Fisrt name:"); let firstName = String(readLine() ?? "")
 print("Last name:"); let lastName = String(readLine() ?? "")
 print("----------------------------------")
-print("Berikut ini adalah operator-operator yang digunakan:")
+print("List operator should be use:")
 
 // Operator Concatenation
 let concatenationString = firstName + lastName
@@ -35,47 +35,47 @@ if firstName == lastName {
 }
 print("3. String Comparison Operator : \"\(comparisonString)\"")
 print("----------------------------------")
-print("Berikut ini adalah fungsi-fungsi yang digunakan:")
+print("List function should be use:")
 
-// Menggabungkan nama depan dan nama belakang.
+// First and Last Name
 let fullName = firstName + " " + lastName
 
-// Fungsi isEmpty
+// Function isEmpty
 let empty = firstName.isEmpty || lastName.isEmpty
-print("1. Fungsi isEmpty : \"\(empty)\"")
+print("1. Function isEmpty : \"\(empty)\"")
 
-// Fungsi startIndex
+// Function startIndex
 let startIndex = fullName[fullName.startIndex]
-print("2. Fungsi startIndex : \"\(startIndex)\"")
+print("2. Function startIndex : \"\(startIndex)\"")
 
-// Fungsi index
+// Function index
 let index = fullName[fullName.index(after: fullName.startIndex)]
-print("3. Fungsi index : \"\(index)\"")
+print("3. Function index : \"\(index)\"")
 
-// Fungsi index Lainnya
+// Another index Function
 let customIndex = fullName[fullName.index(fullName.startIndex, offsetBy: 5)]
 print("4. Index ke lima : \"\(customIndex)\"")
 
-// Fungsi endIndex
+// Function endIndex
 let endIndex = fullName[fullName.index(before: fullName.endIndex)]
-print("5. Fungsi endIndex : \"\(endIndex)\"")
+print("5. Function endIndex : \"\(endIndex)\"")
 
-// Fungsi insert
+// Function insert
 var insert = fullName
 insert.insert("!", at:insert.endIndex)
-print("6. Fungsi insert : \"\(insert)\"")
+print("6. Function insert : \"\(insert)\"")
 
-// Fungsi remove
+// Function remove
 var remove = insert
 remove.remove(at: remove.index(before: remove.endIndex))
-print("7. Fungsi remove : \"\(remove)\"")
+print("7. Function remove : \"\(remove)\"")
 
-// Fungsi append
+// Function append
 var append = firstName
 append.append(lastName)
-print("8. Fungsi append : \"\(append)\"")
+print("8. Function append : \"\(append)\"")
 
-// Fungsi count
+// Function count
 let count = fullName.count
-print("9. Fungsi length : \(count)")
+print("9. Function length : \(count)")
 print("----------------------------------")
